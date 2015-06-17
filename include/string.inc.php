af0a054fc7f0163b8001323ed39ccd745ea50716
unknown <C:\ProgramData\Application Data\The Bat!>
2015-06-17 20:05:58 +0300

    lab2

--- include/string.inc.php
+++ include/string.inc.php
@@ -1,9 +1,12 @@
 <?php
-    function Last( $str )
-    {
-        if ( $str== '' )
-        {
-             $str = 'empty string';
-        }
-        return $str;
+    function Last( $str ){
+        return ($str == '' || $str == null) ? 'empty string' : $str[strlen($str)-1];
     }
+
+	function WithoutLast( $str ){
+		return ($str == '' || $str == null) ? 'empty string' : substr($str, 0, (strlen($str)-1));
+	}
+
+	function Reverse( $str ){
+		return ($str == '' || $str == null) ? 'empty string' : strrev($str);
+	}
---
+++ reverse.php
@@ -0,0 +1,3 @@
+<?php
+    require_once("include/common.inc.php");
+    echo Reverse($_GET['str']);
